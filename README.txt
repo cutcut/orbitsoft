0802f55d6e36de106a121163f3af863a1e0921c9
cutcut <crinoline@mail.ru>
2016-01-20 12:38:53 +0300

    Update

---
+++ README.txt
@@ -0,0 +1,2 @@
+Дамп бд в orbitsoft.sql
+Файл конфигурации подключения к server\config\db.php
--- server/controllers/BooksController.php
+++ server/controllers/BooksController.php
@@ -18,38 +18,31 @@ class BooksController extends ActiveController
 		return $actions;
     }
 	
-    public function actionIndex() {
-		$order_field = isset($_GET['order']) && $_GET['order'] ? $_GET['order'] : 'id';
-		$order_type = isset($_GET['order_type']) && $_GET['order_type'] ? ($_GET['order_type'] == 'DESC' ? SORT_DESC : SORT_ASC) : SORT_ASC;
+    public function actionIndex($order = false, $order_type = false, $search_key = false) {
+		
+		$order_field = $order ? $order : 'id';
+		$order_type = $order_type ? ($order_type == 'DESC' ? SORT_DESC : SORT_ASC) : SORT_ASC;
 	
-		if(isset($_GET['search_key']) && $_GET['search_key']) {
+		if($search_key) {
 			$query = Books::find()->andFilterWhere(['or',
-				['like', 'author', $_GET['search_key']],
-				['like', 'name', $_GET['search_key']]]);
+				['like', 'author', $search_key],
+				['like', 'name', $search_key]
+			]);
 		}
 		else $query = Books::find();
 
         return new ActiveDataProvider([
             'query' => $query,
-			/*
-			'pagination' => [
-				'pageSize' => 10,
-			],
-			*/
 			'sort' => ['defaultOrder' => [$order_field => $order_type]]
 
         ]);
     }
 	
 	public function actionUpload($id) {
-		/*
-		$f = fopen ('test_file_upload.log', "a+");
-		fwrite($f, print_r($_FILES, 1));
-		fclose($f);
-		*/
 		if (($model = Books::findOne($id)) !== null) {
 			$file = UploadedFile::getInstanceByName('file');
 			if ($file) $file->saveAs($model->getCoverPath());
+			else throw new NotFoundHttpException('Failed to load the file.');
 		} else {
 			throw new NotFoundHttpException('The book does not exist.');
 		}
